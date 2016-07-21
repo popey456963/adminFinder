832d279cbb26633d66252305193741d5a9b298eb
Popey Gilbert <popey@debenclipper.com>
2016-07-22 00:27:52 +0100

    New Version

--- index.js
+++ index.js
@@ -1,259 +1,371 @@
-var fs = require('fs')
 var request = require('request')
-var _ = require('underscore')
-var atob = require('atob')
-var moment = require('moment')
+var fs = require('fs')
 var express = require('express')
-var parseString = require('xml2js').parseString
-var persistant = require('./data.json')
-var staffInformation = require('./staff.json')
+var SteamID = require('steamid')
+var moment = require('moment')
 
 var app = express()
 
 app.set('view engine', 'pug')
 app.use(express.static('static'))
 
-var positions = ["Senators", "Senior Admin", "admin", "Senior Moderator", "moderator", "Guardian", "Trial Moderator", "informer"]
-var groupLayout = {}
-var useLocal = true
+var groups = ['informer', 'Trial Moderator', 'Guardian', 'moderator', 'Senior Moderator', 'admin', 'Senior Admin', 'Senators']
+var groupNames = ['Informers', 'T. Mods', 'Guardians', 'Mods', 'S. Mods', 'Admins', 'S. Admins', 'Senators']
+var groupNamess = ['Informer', 'T. Mod', 'Guardian', 'Mod', 'S. Mod', 'Admin', 'S. Admin', 'Senator']
+var oldData = require('./admins.json')
+var lockoutTime = 1111111115
+var updates = {}
+var profiles = []
+var playtimes = []
+var steam642 = []
+var steam3 = []
+var times = {}
+var timeskeys = []
+// profiles looks like [['steamid',cd admins 'url'], ['steamid', 'url']]
 
-for (i in positions) {
-  groupLayout[positions[i]] = {}
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})
 }
 
-function title(str) {
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-  })
-}
-
-var sort_object = function(map) {
-    // var keys = _.sortBy(_.keys(map), function(a) { return 1000000000000 - parseFloat(a.replace("-", "")) })
-    var keys = _.sortBy(_.keys(map), function(a) { 
-      var day = moment(a)
-      return day.unix() * -1
-    })
-    var newmap = {}
-    _.each(keys, function(k) {
-        newmap[k] = map[k]
-    })
-    return newmap
-}
-
-Array.prototype.unique = function() {
-  var a = this.concat()
-  for (var i = 0; i < a.length; ++i) {
-    for (var j = i + 1; j < a.length; ++j) {
-      if (a[i] === a[j])
-        a.splice(j--, 1)
-    }
+function colourRank(rank) {
+  if (groups.indexOf(rank) > - 1) {
+    rank = groupNamess[groups.indexOf(rank)]
+  } else if (rank == "") {
+    return "<span style='color: #000000'>Normal</span>"
+  } else {
+    console.log(rank)
+    return "<span style='color: #000000'>" + rank + "</span>"
   }
-  return a
-}
-
-String.prototype.replaceAt = function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
-}
-
-function formatDate(input) {
-  var datePart = input.match(/\d+/g),
-    year = datePart[0].substring(2), // get only two digits
-    month = datePart[1],
-    day = datePart[2]
-  return year + '-' + month + '-' + day
-}
-
-function formatRank(rank) {
   var colours = {
-    "Senators": "#6633ff",
-    "Senior Admin": "#cc3333",
+    "Senator": "#6633ff",
+    "S. Admin": "#cc3333",
     "Admin": "#ff3333",
-    "Senior Moderator": "#3333ff",
-    "Moderator": "#3366ff",
+    "S. Moderator": "#3333ff",
+    "Mod": "#3366ff",
     "Guardian": "#ff33ff",
-    "Trial Moderator": "#3399ff",
+    "T. Moderator": "#3399ff",
     "Informer": "#33cc33"
   }
   return "<span style='color: " + colours[rank] + "'>" + rank + "</span>"
 }
 
-function resolveName(auth) {
-  if (_.has(staffInformation, auth)) {
-    return staffInformation[auth]
-  } else {
-    console.log("Adding passive check to: " + auth)
-    passiveAddName(auth)
-  }
+function formatPlayer(name) {
+  return "<span>" + name + "</span>"
 }
 
-function passiveAddName(auth) {
-  request('https://steamid.eu/api/convert.php?input=' + auth, function(err, response, body) {
-    parseString(body, function (err, result) {
+function ReverseObject(Obj){
+    var TempArr = []
+    var NewObj = {}
+    for (var Key in Obj){
+        TempArr.push(Key)
+    }
+    for (var i = TempArr.length-1; i >= 0; i--){
+        NewObj[TempArr[i]] = Obj[TempArr[i]]
+    }
+    return NewObj
+}
 
-      data = result["convert"]["converted"][0]
-
-      account = data["steam3"][0]
-      account = account.replace("]", "")
-      account = account.split(":")
-      account = account[account.length - 1]
-
-      userInformation = {
-        "AccountID": account,
-        "SteamID2": data["steamid"][0],
-        "SteamID3": data["steam3"][0],
-        "SteamID64": data["steamid64"][0]
-      }
-
-      if (!_.has(staffInformation, auth)) {
-        staffInformation[auth] = userInformation
-        passiveAddPicture(auth, userInformation["SteamID64"])
-        fs.writeFile("staff.json", JSON.stringify(staffInformation, null, 4))
-        console.log("Added passive name: " + JSON.stringify(userInformation))
-      }   
-    });
-  })
-
-
-/*
-  console.log(auth)
-  attempts = [auth, auth.replaceAt(6, "1"), auth.replaceAt(6, "1").replaceAt(8, "0"), 
-              auth.replaceAt(6, "0"), auth.replaceAt(6, "0").replaceAt(8, "1"),
-              auth.replaceAt(8, "0"), auth.replaceAt(8, "1")]
-  for (attemptNo=0; attemptNo<attempts.length; attemptNo++) {
-    request('https://clwo.eu/jailbreak/api/v1/accounts.php?key=*&value=' + attempts[attemptNo], function (error, response, body) {
+function getStaff(callback) {
+  request('http://fastdl.sinisterheavens.com/admins.php', function(error, response, body) {
+    if (!error && response.statusCode == 200) { 
+      var data = []
       try {
-        if (!error && response.statusCode == 200) {
-          if (body.substring(0, 25).indexOf("<!DOCTYPE html>") == -1) {
-            var json = JSON.parse(body).data
-            if (String(JSON.stringify(json)) != "[]") {
-              if (!_.has(staffInformation, auth)) {
-                staffInformation[auth] = json[Object.keys(json)[0]]
-                passiveAddPicture(auth, json[Object.keys(json)[0]]["SteamID64"])
-                fs.writeFile("staff.json", JSON.stringify(staffInformation, null, 4))
-                console.log("Added passive name: " + JSON.stringify(json))
-              }
-            }
-          }
-        }
+        data = JSON.parse(body)
       } catch(e) {
         console.log(e)
       }
-    })
+      if (data == []) {
+        callback(false)
+      } else {
+        callback(data)
+      }
+    }
+  })
+}
+
+function parseStaff(data, callback) {
+  var staff = {}
+  for (var i = 0; i < data.length; i++) {
+    var customName = null
+    var profile = null
+    var history = [[parseInt(new Date().getTime() / 1000), "", data[i].srv_group]]
+    var playtime = 0
+    var steam64 = null
+    var steam3 = null
+    if (oldData[data[i].authid]) {
+      if (oldData[data[i].authid].customName) {
+        customName = oldData[data[i].authid].customName
+      }
+      if (oldData[data[i].authid].steam64) {
+        steam64 = oldData[data[i].authid].steam64
+      } else {
+        packupSteam(data[i].authid, function(newID) {
+          passiveAddSteam64(data[i].authid, newID)
+        })
+      }
+      if (oldData[data[i].authid].profile) {
+        profile = oldData[data[i].authid].profile
+      } else {
+        if (oldData[data[i].authid].user != 'CONSOLE') {
+          packupSteam(data[i].authid, function(newID) {
+            passiveAddPicture(data[i].authid, newID)
+          })
+        }
+      }
+      if (oldData[data[i].authid].steam3) {
+        steam3 = oldData[data[i].authid].steam3
+      } else {
+        packupSteam3(data[i].authid, function(newID) {
+          passiveAddSteam3(data[i].authid, newID)
+        })
+      }
+      /*
+      if (oldData[data[i].authid].playtime) {
+        playtime = oldData[data[i].authid].playtime
+      } else {
+        if (oldData[data[i].authid].user != 'CONSOLE') {
+          console.log(data[i].authid)
+          playtime = cachedAddPlaytime(data[i].authid)
+          console.log(playtime)
+        }
+      }
+      */
+      if (oldData[data[i].authid].history) {
+        history = oldData[data[i].authid].history
+      }
+    }
+    staff[data[i].authid] = {
+      user: data[i].user,
+      group: data[i].srv_group,
+      history: history,
+      customName: customName,
+      profile: profile,
+      steam64: steam64,
+      steam3: steam3
+    }
   }
-*/
+  callback(staff)
+}
+
+function packupSteam(steam, callback) {
+  if (steam != "STEAM_ID_SERVER") {
+    var sid = new SteamID(steam)
+    callback(sid.getSteamID64())
+  }
+}
+
+function packupSteam3(steam, callback) {
+  if (steam != "STEAM_ID_SERVER") {
+    var sid = new SteamID(steam)
+    callback(sid.getSteam3RenderedID())
+  }
+}
+
+function passiveAddSteam64(steamid, steam64) {
+  steam642.push([steamid, steam64])
+}
+
+function passiveAddSteam3(steamid, steam64) {
+  steam3.push([steamid, steam64])
 }
 
 function passiveAddPicture(auth, steamID) {
-  console.log("Added passive picture: " + steamID)
-  var key = atob("MjEyNzlCNzdFNjFCOTI3MEQ1MDVEODEyNDYzRDFDOTA=")
-  var base = "http://api.steampowered.com"
-  request(base + "/ISteamUser/GetPlayerSummaries/v0002/?key=" + key + "&steamids=" + steamID, function (error, response, body) {
+  // console.log('Added passive picture: ' + steamID)
+  var key = '21279B77E61B9270D505D812463D1C90'
+  var base = 'http://api.steampowered.com'
+  var requestURL = base + '/ISteamUser/GetPlayerSummaries/v0002/?key=' + key + '&steamids=' + steamID
+  request(requestURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var profilePicture = JSON.parse(body)["response"]["players"][0]["avatarfull"]
-      staffInformation[auth]["Picture"] = profilePicture
-      fs.writeFile("staff.json", JSON.stringify(staffInformation, null, 4))
-    }
-  })
-}
-
-function resolveSteamID(auth) {
-  var name, picture;
-  if (_.has(staffInformation, auth[0])) {
-    if (_.has(staffInformation[auth[0]], "Name")) {
-      name = staffInformation[auth[0]]["Name"]
-    } else {
-      name = staffInformation[auth[0]]["SteamID2"]
-    }
-    if (_.has(staffInformation[auth[0]], "Picture")) {
-      picture = staffInformation[auth[0]]["Picture"]
-    } else {
-      picture = "nothing.png"
-    }
-
-    auth.push(name)
-    auth.push(picture)
-
-    return auth
-  } else {
-    console.log("Adding passive check to: " + auth)
-    passiveAddName(auth[0])
-    return auth
-  }
-}
-
-function checkAllProfiles() {
-  for(i in staffInformation) {
-    passiveAddPicture(i, staffInformation[i]['SteamID64'])
-  }
-}
-
-// checkAllProfiles()
-
-function update(callback) {
-  simpleGroups = {}
-  request('http://fastdl.sinisterheavens.com/admins.php', function(error, response, body) {
-    var data = JSON.parse(body)
-    for (i in groupLayout) {
-      groupLayout[i] = {}
-    }
-    for (i = 0; i < data.data.length; i++) {
-      simpleGroups[data.data[i]["authid"]] = data.data[i]['srv_group']
-      if (positions.indexOf(data.data[i]['srv_group']) > -1) {
-        groupLayout[data.data[i]['srv_group']][data.data[i]["authid"]] = resolveName(data.data[i]["authid"])
+      try {
+        var profilePicture = JSON.parse(body).response.players[0].avatarfull
+        profiles.push([auth, profilePicture])
+      } catch(e) {
+        console.log('Error Requesting Profile URL: ' + requestURL)
+        throw e
       }
     }
-    callback(simpleGroups)
   })
 }
 
-update(function(simpleGroups) {
-  var oldSimpleGroups = simpleGroups
-  setInterval(function() {
-    update(function(simpleGroups) {
-      var differences = _.omit(simpleGroups, function(v, k) {
-        return oldSimpleGroups[k] === v
-      })
-      var previous = []
-      for (key in differences) {
-        if (positions.indexOf(oldSimpleGroups[key]) < positions.indexOf(simpleGroups[key]) && positions.indexOf(simpleGroups[key]) != -1) {
-          var msg = resolveSteamID([key, "demoted", title(oldSimpleGroups[key]), title(simpleGroups[key])])
-        } else {
-          var msg = resolveSteamID([key, "promoted", title(oldSimpleGroups[key]), title(simpleGroups[key])])
+function passiveAddPlaytime(steamID) {
+  console.log("Request Playtime for User: " + steamID.slice(10))
+  var url = "https://clwo.eu/jailbreak/api/v1/playtime.php?parent=" + steamID.slice(10)
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      try {
+        var json = JSON.parse(body)
+        if (json.data && json.data[steamID.slice(10)] && json.data[steamID.slice(10)].onteam) {
+          var playTime = json.data[steamID.slice(10)].onteam
+          playtimes.push([steamID, playTime])
         }
-        // console.log(msg)
-        previous.push(msg)
+      } catch(e) {
+        console.log('Error Requesting Playtime URL: ' + url)
+        throw e
       }
-      var date = formatDate(new Date().toJSON().slice(0, 10))
-      if (_.has(persistant, date)) {
-        persistant[date] = persistant[date].concat(previous)
+    } else {
+      console.log('Error Requesting Playtime URL: ' + url)
+    }
+  })
+}
+
+function passiveAddCachedPlaytime(steamID) {
+  console.log("This was called")
+  if (timeskeys.indexOf(steamID.slice(10)) > -1) {
+    callback(times[steamID.slice(10)].onteam)
+  }
+}
+
+function spotDifferences(staff, callback) {
+  staff = interceptData(staff)
+  if (oldData != {}) {
+    var staffKeys = Object.keys(staff)
+    for (var i = 0; i < staffKeys.length; i++) {
+      if (oldData[staffKeys[i]] != undefined) {
+        if (staff[staffKeys[i]]['user'] != oldData[staffKeys[i]]['user']) {
+          console.log('Username updated from ' + oldData[staffKeys[i]]['user'] + ' to ' + staff[staffKeys[i]]['user'] + ' for user ' + oldData[staffKeys[i]]['user'])
+        }
+        if (staff[staffKeys[i]]['group'] != oldData[staffKeys[i]]['group']) {
+          console.log('Group updated from ' + oldData[staffKeys[i]]['group'] + ' to ' + staff[staffKeys[i]]['group'] + ' for user ' + oldData[staffKeys[i]]['user'])
+          staff[staffKeys[i]]['history'].push([parseInt(new Date().getTime() / 1000), oldData[staffKeys[i]]['group'], staff[staffKeys[i]]['group']])
+        }
       } else {
-        persistant[date] = previous
+        console.log(staff[staffKeys[i]]['user'] + ' has been added to the database.')
       }
-      persistant[date] = persistant[date].unique()
-      fs.writeFile("data.json", JSON.stringify(persistant, null, 4))
-        // console.log(persistant)
-      oldSimpleGroups = simpleGroups
-        /*resolveAllNames(groupLayout, function(data) {
-          groupLayout = data
-        })*/
+    }
+    oldData = staff
+    saveStaff(staff)
+  }
+  else {
+    oldData = staff
+    saveStaff(staff)
+  }
+  callback()
+}
+
+function interceptData(staff) {
+  for (var i = 0; i < profiles.length; i++) {
+    staff[profiles[i][0]].profile = profiles[i][1]
+  }
+  for (var i = 0; i < playtimes.length; i++) {
+    staff[playtimes[i][0]].playtime = playtimes[i][1]
+  }
+  for (var i = 0; i < steam642.length; i++) {
+    staff[steam642[i][0]].steam64 = steam642[i][1]
+  }
+  for (var i = 0; i < steam3.length; i++) {
+    staff[steam3[i][0]].steam3 = steam3[i][1]
+  }
+  profiles = []
+  playtimes = []
+  steam642 = []
+  steam3 = [] 
+  return staff
+}
+
+function saveStaff(staff) {
+  fs.writeFile('./admins.json', JSON.stringify(staff, null, 4), function(err) {
+    if(err) {
+      return console.log(err)
+    }
+  })
+}
+
+function updatePlaytimes() {
+  console.log("Updating Playtimes")
+  var url = "https://clwo.eu/jailbreak/api/v1/playtime.php"
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      try {
+        times = JSON.parse(body).data
+        timeskeys = Object.keys(times)
+        console.log("Playtimes Updated")
+      } catch(e) {
+        console.log('Error Requesting Playtime URL: ' + url)
+        throw e
+      }
+    } else {
+      console.log('Error Requesting Playtime URL: ' + url)
+    }
+  })
+}
+
+function changeFormat() {
+  var groupStructure = {}
+  for (var i = 0; i < groups.length; i++) {
+    groupStructure[groups[i]] = []
+  }
+  for (var player in oldData) {
+    if (groups.indexOf(oldData[player].group) > -1) {
+      var obj = {
+        "user": oldData[player].user,
+        "profile": oldData[player].profile,
+        "steam64": oldData[player].steam64,
+        "steam3": oldData[player].steam3,
+        "steamid": player
+      }
+      if (oldData[player].customName) {
+        obj.user = oldData[player].customName
+      }
+      groupStructure[oldData[player].group].push(obj)
+    }
+  }
+  return groupStructure
+}
+
+function changeUpdate() {
+  updates = {}
+  for (var player in oldData) {
+    var history = oldData[player].history
+    for (var i = 0; i < history.length; i++) {
+      if (history[i][0] > lockoutTime) {
+        var changeLog = [player, "", history[i][1], history[i][2], oldData[player].profile]
+        if (oldData[player].user) {
+          changeLog[0] = oldData[player].user
+        }
+        if (oldData[player].customName) {
+          changeLog[0] = oldData[player].customName
+        }
+        if (groups.indexOf(history[i][1]) > groups.indexOf(history[i][2])) {
+          changeLog[1] = "demoted"
+        } else {
+          changeLog[1] = "promoted"
+        }
+        if (updates[toTitleCase(moment(history[i][0] * 1000).fromNow())] == undefined) {
+          updates[toTitleCase(moment(history[i][0] * 1000).fromNow())] = [changeLog]
+        } else {
+          updates[toTitleCase(moment(history[i][0] * 1000).fromNow())].push(changeLog)
+        }
+      }
+    }
+  }
+  return ReverseObject(updates)
+}
+
+function mainScript() {
+  getStaff(function(data) {
+    parseStaff(data.data, function(staff) {
+      spotDifferences(staff, function() {
+        console.log('Spot Differences Finished')
+      })
     })
-  }, 3000)
-})
+  })
+}
+
+mainScript()
+setInterval(mainScript, 5000)
+updatePlaytimes()
+setInterval(updatePlaytimes, 3600000)
 
 app.get('/', function(req, res) {
   console.log("Someone tried to call the page")
-    // console.log(groupLayout)
-  console.log(persistant)
-  console.log(sort_object(persistant))
   res.render('index', {
-    groups: groupLayout,
-    changes: sort_object(persistant),
-    formatRank: formatRank,
-    toTitleCase: function(str) {
-      return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      })
-    }
+    groups: changeFormat(),
+    updates: changeUpdate(),
+    groupNames: groupNames,
+    groupCodes: groups,
+    colourRank: colourRank,
+    toTitleCase: toTitleCase,
+    formatPlayer: formatPlayer
   })
 })
 
